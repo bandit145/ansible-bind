0e6f64b1825c8dea129e76269ab5af393d61afe8
Philip Bove <pgbson@gmail.com>
2019-03-11 00:42:56 -0400

    tests and working on stunnel

--- molecule/update/tests/test_default.py
+++ molecule/update/tests/test_default.py
@@ -6,9 +6,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_named_running(host):
+    named = host.service('named')
+    assert named.is_running
+    assert named.is_enabled
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_zone_files(host):
+    delete_com = host.file('/var/named/db.delete.com')
+    test_com = host.file('/var/named/db.test.com')
+    assert not delete_com.exists
+    assert test_com.contains('2 ;serial')

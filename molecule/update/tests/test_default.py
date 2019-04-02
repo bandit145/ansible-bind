b4e4e6ab58bec92a472de3824f1885c75ee55ba0
Philip Bove <pgbson@gmail.com>
2019-04-02 17:05:15 -0400

    passing tests in master/slave config

--- molecule/update/tests/test_default.py
+++ molecule/update/tests/test_default.py
@@ -6,16 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_zone_files(host):
-    with host.sudo():
-        assert not host.file('/var/named/db.delete.com').exists
-        test_content = host.file('/var/named/db.test.com').content_string.split('\n') # noqa 501
-        assert test_content[2] == '\t2 ;serial'
-        dynamic_content = host.file('/var/named/db.dynamic.com').content_string.split('\n') # noqa 501
-        assert dynamic_content[2] == '\t1 ;serial'
-        assert dynamic_content[10] == 'combine-record IN A 192.168.1.3'
-
-
 def test_ports(host):
     ports = ['53/udp', '53/tcp']
     output = host.check_output('firewall-cmd --list-ports').split(' ')
--- molecule/update/tests/test_ns2.py
+++ molecule/update/tests/test_ns2.py
@@ -10,5 +10,5 @@ def test_zone_files(host):
     with host.sudo():
         test_com_serial = host.check_output("dig soa test.com @127.0.0.1 | awk 'NR==15{print $7}'") # noqa 501
         assert test_com_serial == '2'
-        dynamic_com_serial = host.check_output("dig soa dyanamic.com @127.0.0.1 | awk 'NR==15{print $7}'") # noqa 501
+        dynamic_com_serial = host.check_output("dig soa dynamic.com @127.0.0.1 | awk 'NR==15{print $7}'") # noqa 501
         assert dynamic_com_serial == '1'
